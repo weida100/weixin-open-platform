fe083b4a80bf8cbd20648dc1770b13249e6288db
Weida <sgenmi@gmail.com>
2023-08-25 00:13:48 +0800

    add:serve

--- src/Response.php
+++ src/Response.php
@@ -9,9 +9,13 @@ declare(strict_types=1);
 
 namespace Weida\WeixinOpenPlatform;
 
-use Weida\WeixinCore\Message;
+use Psr\Http\Message\ResponseInterface;
+use Weida\WeixinCore\AbstractResponse;
+use Weida\WeixinCore\Contract\MessageInterface;
+use Weida\WeixinCore\Message as CoreMessage;
+use Weida\WeixinOfficialAccount\Message\Message;
 
-class Response extends \Weida\WeixinCore\Response
+class Response extends AbstractResponse
 {
     /**
      * @param callable|string|array|object $callback
@@ -19,7 +23,7 @@ class Response extends \Weida\WeixinCore\Response
      * @author Weida
      */
     public function handleAuthorized(callable|string|array|object $callback):static{
-        $this->addEventListener(Message::OPEN_AUTHORIZED,$callback);
+        $this->addEventListener(CoreMessage::OPEN_AUTHORIZED,$callback);
         return $this;
     }
 
@@ -29,7 +33,7 @@ class Response extends \Weida\WeixinCore\Response
      * @author Weida
      */
     public function handleAuthorizeUpdated(callable|string|array|object $callback):static{
-        $this->addEventListener(Message::OPEN_UPDATEAUTHORIZED,$callback);
+        $this->addEventListener(CoreMessage::OPEN_UPDATEAUTHORIZED,$callback);
         return $this;
     }
 
@@ -39,7 +43,40 @@ class Response extends \Weida\WeixinCore\Response
      * @author Weida
      */
     public function handleUnauthorized(callable|string|array|object $callback):static{
-        $this->addEventListener(Message::OPEN_UNAUTHORIZED,$callback);
+        $this->addEventListener(CoreMessage::OPEN_UNAUTHORIZED,$callback);
         return $this;
     }
+
+    /**
+     * @return ResponseInterface
+     * @author Weida
+     */
+    public function serve():ResponseInterface{
+        if (!empty($this->params['echostr'])) {
+            $this->withBody($this->params['echostr']);
+            return $this;
+        }
+        $message = $this->getDecryptedMessage();
+        $response = $this->middleware->handler($this,$message);
+        if(empty($response)){
+            return $this;
+        }
+        if(is_string($response) || is_numeric($response)){
+            $response = Message::Text((string)$response);
+        }elseif (is_array($response)){
+            if(!empty($response['msgtype']) || !empty($response['MsgType'])){
+                $response = new Message($response);
+            }
+        }
+        if ($response instanceof MessageInterface){
+            $this->withHeader('Content-Type', 'application/xml;charset=utf-8');
+            if(!($response instanceof Message)){
+                $response = new Message($response->getAttributes());
+            }
+            $content = $response->toXmlReply($message,$this->encryptor);
+            $this->withBody($content);
+        }
+        return $this;
+    }
+
 }
